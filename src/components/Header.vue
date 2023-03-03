a73e5a08fdb7f4e734e78fcb7e4bb6e480e4fd6a
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-03 18:01:38 +0100

    FEAT change titol

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,13 @@ export default {
 </script>
 <template>
     <div class="container">
-        <h1>TITOLONE GRANDE</h1>
+        <h1>yu-gi-oh</h1>
     </div>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h1 {
+    text-transform: uppercase;
+    text-align: center;
+    padding: 20px 10px;
+}
+</style>

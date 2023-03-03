581059e3de4b88536287f9b234a7e66e0232d7cd
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-03 15:54:44 +0100

    FEAT create function for get info

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,9 +1,25 @@
 <script>
+import axios from 'axios'
 export default {
-
+    data() {
+        return {
+            Card: [],
+        }
+    },
+    methods: {
+        getCards() {
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+                .then((res) => {
+                    console.log(res)
+                })
+        }
+    },
+    created() {
+        this.getCards()
+    }
 }
 </script>
 <template>
     <h2>spazione main con carte</h2>
 </template>
-<style></style>
+<style lang="scss" scoped></style>

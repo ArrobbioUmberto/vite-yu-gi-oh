617fe7b20bf2b01b74a9261a46b3254f6c7ac16b
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-03 17:16:29 +0100

    FEAT create card vue element

---
+++ src/components/Cards.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    props: {
+        el: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+<template>
+    <li>
+        <img :src="el.card_images[0].image_url" alt="">
+        <h3>{{ el.name }}</h3>
+    </li>
+</template>
+<style lang="scss" scoped>
+li {
+    background-color: rgba($color: blue, $alpha: 0.5);
+}
+
+h3 {
+    color: white;
+    font-size: 20px;
+    font-weight: 500;
+    text-align: center;
+}
+</style>

1e3c6598b8c7c12ec633050604bd19daa5b07fff
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-03 18:01:30 +0100

    FEAT add archetype

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -12,6 +12,7 @@ export default {
     <li>
         <img :src="el.card_images[0].image_url" alt="">
         <h3>{{ el.name }}</h3>
+        <h4>{{ el.archetype }}</h4>
     </li>
 </template>
 <style lang="scss" scoped>
@@ -24,5 +25,13 @@ h3 {
     font-size: 20px;
     font-weight: 500;
     text-align: center;
+}
+
+h4 {
+    text-align: center;
+    color: yellow;
+    font-weight: 300;
+    font-style: italic;
+    padding: 5px;
 }
 </style>
